008e793f1c5d4dd232aac3a4a3e6a2e5269b8996
Brett Gibbons <brettgibbons44@gmail.com>
2023-11-15 20:16:22 -0400

    fixing everything

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if($_SERVER['REQUEST_METHOD']=== 'POST'){
     registerUser($email,$password);
 }
 function registerUser($email,$password){
-    $welcomeMessage = "Welcome your email is $email,your password is $password";
+    $welcomeMessage = "Welcome your email is $email";
     
     sendWelcomeEmail($welcomeMessage);
     header('location:welcome.php');
@@ -21,15 +21,15 @@ function sendWelcomeEmail($message){
     //Server settings
     $mail->SMTPDebug = 1;
     $mail->isSMTP();
-    $mail->Host       = 'smtp.mailgun.org';//Must create a profile with mailgun to use 
+    $mail->Host       = '';//Must create a profile with mailgun to use 
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'postmaster@sandboxf2f2ea2ce27440c89f16b71d40bca460.mailgun.org';//username copied from mailgun
-    $mail->Password   = 'd8358de9d67334ac40180546d6f20a19-8c9e82ec-bf5e6c35';//password copied from mailgun
+    $mail->Username   = '';//username copied from mailgun
+    $mail->Password   = '';//password copied from mailgun
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use ENCRYPTION_SMTPS for port 465
     $mail->Port       = 587; // or 465
     //Recipients
-    $mail->setFrom('random@gmail.com', 'Mailer');//email thats the sender
-    $mail->addAddress('brettgibbons44@gmail.com');//change this to your email
+    $mail->setFrom("");//email thats the sender
+    $mail->addAddress('');//change this to your email
     //Content
     $mail->isHTML(true);
     $mail->Subject = 'Here is the subject';// this is your subject
